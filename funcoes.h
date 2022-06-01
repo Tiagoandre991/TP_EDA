8616a8d0bc2ab6ff7f59f31d04e8114fe0073e88
Tiagoandre991 <95867590+Tiagoandre991@users.noreply.github.com>
2022-06-01 21:27:21 +0100

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct Lista {  // estrura de dados para a constituicao de um job
+typedef struct Lista {  
     int job;
     int operacao;
     int maquina;
@@ -14,15 +14,15 @@ int alterarOperacao(Lista* listaJ, int job, int op, int opNova);
 int alterarMaquina(Lista* listaJ, int job, int op, int maq, int maqNova);
 int alterarTempo(Lista* listaJ, int job, int op, int maq, int temp);
 int verificarJob(Lista* listaJ, int job);
-int verificarOperacao(Lista* listaJ, int job, int op); // ver se a operacao existe
-int verificarMaquina(Lista* listaJ, int job, int op, int maq); // ver se a maquina existe
+int verificarOperacao(Lista* listaJ, int job, int op); 
+int verificarMaquina(Lista* listaJ, int job, int op, int maq); 
 int verMaior(Lista* listaJ);
 int verMenor(Lista* listaJ);
 int verMedia(Lista* listaJ);
-void escreverFicheiro(Lista* listaJ); // escrever os dados do ficheiro
-void listarJob(Lista* listaJ); // listar os elementos da lista
-Lista* inserirMaquina(Lista* listaJ, int job, int op, int maq, int temp); // inserir os dados no meio
+void escreverFicheiro(Lista* listaJ); 
+void listarJob(Lista* listaJ); 
+Lista* inserirMaquina(Lista* listaJ, int job, int op, int maq, int temp); 
 Lista* removerJob(Lista* listaJ, int job);
 Lista* removerOperacao(Lista* listaJ, int job, int op);
-Lista* removerMaquina(struct Lista** aux, int job, int op, int maq);// remover um objeto passando por parametro a sua operacao e a maquina
-Lista* lerFicheiro(Lista* listaJ); // ler os dados no ficheiro
+Lista* removerMaquina(struct Lista** aux, int job, int op, int maq);
+Lista* lerFicheiro(Lista* listaJ); 
